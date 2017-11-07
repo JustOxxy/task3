f85f388d1bcf7b77e1c02dbfb79e2f14e411f06a
JustOxxy <oksundel@mail.ru>
2017-11-07 18:04:16 +0300

    Finish devise user, views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  root            'static_pages#home'
 
-  get 'about'  => 'static_pages#about'
+  root             'static_pages#home'
 
-  get'contact' => 'static_pages#contact'
+  get 'about'   => 'static_pages#about'
+
+  get 'contact' => 'static_pages#contact'
+
+  devise_for :users
+
+  resources :users, only: [:show, :edit]
+  # get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
